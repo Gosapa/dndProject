b5c17c96543fbf657dca243c8045abe1529e4e29
Ian Park <ianpark0412@gmail.com>
2024-07-12 05:22:57 +0900

    added home

--- gemini/functions.py
+++ gemini/functions.py
@@ -6,8 +6,8 @@ model = config.model
 
 def create_stat(name="Jake", origin="house", gender="male", plot="warrier"):
     prompt = prompts.character_creation_prompt(name,origin,gender,plot)
-    print(prompt)
+    print(prompt) # DEBUG
     raw_response = model.generate_content(prompt, safety_settings=config.safety_settings)
     response = json.loads(raw_response.text)
-    print(response)
+    print(response) # DEBUG
     return response
